00b002e3b0ddf1efb98034ed45b753a4698a1df0
ukyeong-user <hyk040520@naver.com>
2025-10-30 20:31:09 +0900

    style: 메인 패널 레이아웃 및 색상 변경

---
+++ src/pages/MakerPage/MainPanel/PromptTitleInput.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+function PromptTitleInput() {
+  const [title, setTitle] = useState("");
+
+  return (
+    <TitleInputWrapper>
+      <TitleInput
+        type="text"
+        placeholder="프롬프트 제목을 입력하세요."
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <Underline />
+    </TitleInputWrapper>
+  );
+}
+
+export default PromptTitleInput;
+
+const TitleInputWrapper = styled.div`
+  width: 100%;
+  max-width: 401px;
+  position: relative;
+`;
+
+const TitleInput = styled.input`
+  font-family: "Pretendard Variable", sans-serif;
+  font-size: 2.25rem;
+  font-weight: 600;
+  color: #aadff7;
+  background: transparent;
+  border: none;
+  outline: none;
+  padding: 0.5rem 0;
+
+  &::placeholder {
+    color: #e1e1e0;
+  }
+
+  &:focus {
+    color: #aadff7;
+  }
+`;
+
+const Underline = styled.div`
+  width: 22.5vw;
+  height: 2px;
+  background-color: #f2f2f2;
+  margin-top: 0.5vh;
+`;
