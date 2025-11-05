e4d6b6020f2061b512fe1df11682e10236386e7f
ukyeong-user <hyk040520@naver.com>
2025-11-05 23:18:53 +0900

    design: 자잘한 css 레이아웃 수정

--- src/pages/MakerPage/SidePanel/UpgradeCard.jsx
+++ src/pages/MakerPage/SidePanel/UpgradeCard.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import UpgradeMenu from "../shared/UpgradeMenu";
 
-function UpgradeCard({ title, content, isApplied }) {
+export default function UpgradeCard({ title, content, isApplied }) {
   return (
     <CardWrapper>
       <CardTitle $isApplied={isApplied}>{title}</CardTitle>
@@ -13,30 +12,28 @@ function UpgradeCard({ title, content, isApplied }) {
   );
 }
 
-export default UpgradeCard;
-
 // Styled Components
 const CardWrapper = styled.div`
-  width: 100%;
-  margin-bottom: 16px;
+  width: 18.91vw; /* 363px @ 1920px */
+  margin-bottom: 1rem; /* 16px */
   position: relative;
 `;
 
 const CardTitle = styled.p`
   font-family: "Pretendard Variable", sans-serif;
-  font-size: 23px;
+  font-size: 1.4375rem; /* 23px */
   font-weight: 600;
   line-height: normal;
   color: ${(props) => (props.$isApplied ? "#00aeff" : "#000000")};
-  margin: 0 0 16px 0;
+  margin: 0 0 1rem 0; /* 16px */
 `;
 
 const CardContentWrapper = styled.div`
   width: 100%;
-  height: 167px;
+  height: 15.46vh; /* 167px @ 1080px */
   background-color: ${(props) => (props.$isApplied ? "#001e40" : "#e0f5ff")};
-  border-radius: 16px;
-  padding: 26px 21px;
+  border-radius: 0.83vw; /* 16px @ 1920px */
+  padding: 2.41vh 1.09vw; /* 26px 21px @ 1920x1080 */
   overflow: hidden;
   cursor: pointer;
   transition: opacity 0.2s;
@@ -47,10 +44,10 @@ const CardContentWrapper = styled.div`
 `;
 
 const CardContent = styled.div`
-  width: 100%;
-  height: 115px;
+  width: 16.72vw; /* 321px @ 1920px */
+  height: 10.65vh; /* 115px @ 1080px */
   font-family: "Pretendard Variable", sans-serif;
-  font-size: 23px;
+  font-size: 1.4375rem; /* 23px */
   font-weight: 400;
   line-height: normal;
   color: ${(props) => (props.$isApplied ? "#ffffff" : "#000000")};
