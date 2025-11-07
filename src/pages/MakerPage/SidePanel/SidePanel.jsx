58255228365254af505f31c465fad10876ee5487
ukyeong-user <hyk040520@naver.com>
2025-11-07 21:30:59 +0900

    feat: api 요청이 반복적으로 되어서 isOpen props로 관리

--- src/pages/MakerPage/SidePanel/SidePanel.jsx
+++ src/pages/MakerPage/SidePanel/SidePanel.jsx
@@ -6,23 +6,33 @@ import UpgradeSection from "./UpgradeSection";
 
 // props는 나중에 백엔드 연동 시 처리를 위해 선언
 export default function SidePanel({
+  isOpen = true,
   onToggle,
   upgrades,
   onAcceptUpgrade,
   onCancelUpgrade,
   onEditUpgrade,
 }) {
+  const hasUpgrades = Array.isArray(upgrades) && upgrades.length > 0;
+
   return (
-    <SidebarWrapper>
+    <SidebarWrapper $isOpen={isOpen}>
       <TopPanel onClose={onToggle} />
       <ContentWrapper>
-        <UpgradeSection
-          upgrades={upgrades}
-          onAccept={onAcceptUpgrade}
-          onCancel={onCancelUpgrade}
-          onEdit={onEditUpgrade}
-        />
-        <PromptHub />
+        {hasUpgrades ? (
+          <UpgradeOnlyContainer>
+            <UpgradeSection
+              upgrades={upgrades}
+              onAccept={onAcceptUpgrade}
+              onCancel={onCancelUpgrade}
+              onEdit={onEditUpgrade}
+            />
+          </UpgradeOnlyContainer>
+        ) : (
+          <PromptHubContainer>
+            <PromptHub />
+          </PromptHubContainer>
+        )}
       </ContentWrapper>
     </SidebarWrapper>
   );
@@ -32,7 +42,7 @@ const SidebarWrapper = styled.div`
   width: 23.8vw; /* 457px / 1920px */
   height: 100%;
   background-color: #001e40;
-  display: flex;
+  display: ${(props) => (props.$isOpen ? "flex" : "none")};
   flex-direction: column;
   position: relative;
   box-shadow: 5px 0px 26px 0px rgba(0, 0, 0, 0.06);
@@ -40,7 +50,22 @@ const SidebarWrapper = styled.div`
 
 const ContentWrapper = styled.div`
   flex: 1;
-  overflow-y: auto;
   display: flex;
   flex-direction: column;
+  overflow: hidden;
+  background-color: #ffffff;
+`;
+
+const UpgradeOnlyContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+`;
+
+const PromptHubContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
 `;
