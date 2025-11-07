bb27552daa47ce6c2b7b37d11eaa1d74decff66a
ukyeong-user <hyk040520@naver.com>
2025-11-08 02:53:38 +0900

    design: 업그레이드되었을 때 CSS  변경

--- src/pages/MakerPage/SidePanel/UpgradeCard.jsx
+++ src/pages/MakerPage/SidePanel/UpgradeCard.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export default function UpgradeCard({ title, content, isApplied }) {
   return (
-    <CardWrapper>
+    <CardWrapper $isApplied={isApplied}>
       <CardTitle $isApplied={isApplied}>{title}</CardTitle>
       <CardContentWrapper $isApplied={isApplied}>
-        <CardContent $isApplied={isApplied}>{content}</CardContent>
+        <UpgradedText $isApplied={isApplied}>{content}</UpgradedText>
       </CardContentWrapper>
     </CardWrapper>
   );
@@ -21,34 +21,41 @@ const CardWrapper = styled.div`
 
 const CardTitle = styled.p`
   font-family: "Pretendard Variable", sans-serif;
-  font-size: 1.4375rem; /* 23px */
+  font-size: 1.4375rem;
   font-weight: 600;
   line-height: normal;
   color: ${(props) => (props.$isApplied ? "#00aeff" : "#000000")};
-  margin: 0 0 1rem 0; /* 16px */
+  margin: 0 0 1rem 0;
 `;
 
 const CardContentWrapper = styled.div`
   width: 100%;
-  height: 15.46vh; /* 167px @ 1080px */
+  height: 15.46vh;
   background-color: ${(props) => (props.$isApplied ? "#001e40" : "#e0f5ff")};
-  border-radius: 0.83vw; /* 16px @ 1920px */
-  padding: 2.41vh 1.09vw; /* 26px 21px @ 1920x1080 */
+  border-radius: 0.83vw;
+  padding: 2.41vh 1.09vw;
   overflow-y: auto;
   cursor: pointer;
   transition: opacity 0.2s;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
 
   &:hover {
     opacity: 0.9;
   }
 `;
 
-const CardContent = styled.div`
+const baseTextStyles = css`
   width: 100%;
   font-family: "Pretendard Variable", sans-serif;
   font-size: 1.4375rem; /* 23px */
   font-weight: 400;
   line-height: 1.4;
-  color: ${(props) => (props.$isApplied ? "#ffffff" : "#000000")};
   white-space: pre-wrap;
+`;
+
+const UpgradedText = styled.div`
+  ${baseTextStyles}
+  color: ${(props) => (props.$isApplied ? "#ffffff" : "#000000")};
 `;
